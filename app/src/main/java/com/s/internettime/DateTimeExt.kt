8d238bfb57331a9d57b5ef34aed57ba92a5e6627
tosinonikute <tosinmath007@gmail.com>
2020-05-17 08:42:48 +0100

    third commit

--- app/src/main/java/com/s/internettime/DateTimeExt.kt
+++ app/src/main/java/com/s/internettime/DateTimeExt.kt
@@ -7,6 +7,6 @@ import java.util.*
 @JvmOverloads
 fun Date.format(pattern: String, timezone: String = "UTC"): String {
     val format = SimpleDateFormat(pattern, Locale.getDefault())
-    format.timeZone = TimeZone.getTimeZone(timezone)
+    format.timeZone = TimeZone.getTimeZone(timezone) // or we can also use GMT+6:00
     return format.format(this)
 }
